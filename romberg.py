901574318f7d7feb93cc92411b9c3be94b59d458
Daud Dhiya <daud.dhiya31@gmail.com>
2022-12-20 19:11:17 +0700

    Update romberg.py

--- romberg.py
+++ romberg.py
@@ -3,7 +3,10 @@ from scipy import integrate
 def rmbg(x): 
     return 1 / (1 + 9 + x)
 
+print("\n---------- INTEGRASI ROMBERG ----------")
+print("Masukkan batas bawah integrasi : ", end='')
 batas_bawah = int(input())
+print("Masukkan batas atas integrasi : ", end='')
 batas_atas = int(input())
 
 res = integrate.romberg(rmbg, batas_bawah, batas_atas, show=True)
